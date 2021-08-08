57b87455f3072650d84a1277723e45f9dee2b134
KulakDmitry <kulakdima@mail.ru>
2021-08-08 19:59:48 +0300

    update

--- main.js
+++ main.js
@@ -1,58 +1,83 @@
-document.getElementById("btn").addEventListener("click", main);
+const sides = {};
 
-function main() {
-  const sides = {};
-  sides.a = parseInt(document.querySelector("#first-side").value);
-  sides.b = parseInt(document.querySelector("#second-side").value);
-  sides.c = parseInt(document.querySelector("#third-side").value);
+let answerText;
+let smile;
+let answerBlock;
 
-  const low = parseInt(document.querySelector("#low").value);
-  const upp = parseInt(document.querySelector("#upp").value);
+function triangleSides(sides) {}
 
+function renderAnswerBlock() {
   const header = document.querySelector("#app");
-  const answerBlock = document.createElement("div");
+  answerBlock = document.createElement("div");
   header.append(answerBlock);
   answerBlock.className = "title";
+}
+
+function trueTriangle() {
+  answerText = "можно";
+  smile = ":)";
+  answerBlock.style.backgroundColor = "rgb(115, 209, 100)";
+}
+
+function falseTriangle() {
+  answerText = "нельзя";
+  smile = ":(";
+  answerBlock.style.backgroundColor = "red";
+}
+
+function renderAnswer() {
+  answerBlock.innerHTML = `На сторонах ${sides.a}, ${sides.b}, ${sides.c} в пределах от ${low} до ${upp}`;
+  const answerTriangle = document.createElement("p");
+  answerTriangle.innerHTML = `${answerText} построить треугольник ${smile}`;
+  answerBlock.append(answerTriangle);
+}
+
+function inRange() {
+  for (let i in sides) {
+    if (sides[i] > upp || sides[i] < low) {
+      const text = document.createElement("p");
+      text.innerHTML = `сторона ${i} = ${sides[i]}: не может быть использована т.к. должна быть в пределах от ${low} до ${upp} `;
+      answerBlock.style.backgroundColor = "yellow";
+      answerBlock.append(text);
+
+      return false;
+    }
+  }
+  return true;
+}
+
+function isTriangle() {
+  const a = sides.a;
+  const b = sides.b;
+  const c = sides.c;
+
+  a + b > c && a + c > b && b + c > a ? trueTriangle() : falseTriangle();
+
+  if (inRange()) {
+    renderAnswer();
+  }
+}
+
+function getTriangleValue(value) {
+  return parseInt(document.querySelector(value).value);
+}
+
+function event() {
+  sides.a = getTriangleValue("#first-side");
+  sides.b = getTriangleValue("#second-side");
+  sides.c = getTriangleValue("#third-side");
+
+  low = getTriangleValue("#low");
+  upp = getTriangleValue("#upp");
+
+  renderAnswerBlock();
 
   isTriangle();
+}
 
-  function inRange() {
-    for (let i in sides) {
-      if (sides[i] > upp || sides[i] < low) {
-        const text = document.createElement("p");
-        text.innerHTML = `сторона ${i} = ${sides[i]}: не может быть использована т.к. должна быть в пределах от ${low} до ${upp} `;
-        answerBlock.style.backgroundColor = "yellow";
-        answerBlock.append(text);
-
-        return false;
-      }
-    }
-    return true;
-  }
-
-  function isTriangle() {
-    const a = sides.a;
-    const b = sides.b;
-    const c = sides.c;
-    let answerText;
-    let smile;
-    if (a + b > c && a + c > b && b + c > a) {
-      answerText = "можно";
-      smile = ":)";
-      answerBlock.style.backgroundColor = "rgb(115, 209, 100)";
-    } else {
-      answerText = "нельзя";
-      smile = ":(";
-      answerBlock.style.backgroundColor = "red";
-    }
-    if (inRange()) {
-      answerBlock.innerHTML = `На сторонах ${sides.a}, ${sides.b}, ${sides.c} в пределах от ${low} до ${upp}`;
-
-      const answerTriangle = document.createElement("p");
-      answerTriangle.innerHTML = `${answerText} построить треугольник ${smile}`;
-      answerBlock.append(answerTriangle);
-    }
-  }
+function main() {
+  const checkButton = document.getElementById("btn");
+  checkButton.addEventListener("click", event);
 }
 
 export { main };
